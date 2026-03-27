8770fdba6fd656e7d310bd9475aea605e225683e
Nick Efford <nick.efford@gmail.com>
2026-03-27 17:02:47 +0000

    Session 1: added plotting code for Task 3

---
+++ session1/task3/plot.py
@@ -0,0 +1,33 @@
+# Task 3: program to plot benchmarking data
+
+import numpy as np
+import pandas as pd
+import seaborn as sb
+
+
+def plot(data):
+    data.plot(x="size", ylabel="Time (sec)", loglog=False)
+
+
+def log_plot(data):
+    data.plot(x="size", ylabel="Time (sec)", loglog=True)
+
+
+def log_fit(data, column):
+    x = np.log10(data["size"])
+    y = np.log10(data[column])
+    slope, _ = np.polyfit(x, y, deg=1)
+    print(f"Log-log best fit for {column}: slope={slope:.3f}")
+
+
+def linear_regression(data, column):
+    sb.regplot(data, x="size", y=column, order=1)
+
+
+def quadratic_regression(data, column):
+    sb.regplot(data, x="size", y=column, order=2)
+
+
+if __name__ == "__main__":
+    data = pd.read_csv("bubblesort.csv")
+    # call functions here
