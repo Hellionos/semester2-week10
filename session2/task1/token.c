a24a04038895f0ef9ddccf150d5125a97a8a4b37
Ethan West <sqqz0154@leeds.ac.uk>
2026-05-01 07:50:31 +0000

    done token but do't understand completely

--- session2/task1/token.c
+++ session2/task1/token.c
@@ -5,9 +5,22 @@
 int main( void ) {
 
     char buffer[100] = "the quick brown fox jumped over the lazy dog";   // define a string to process
+    const char *delim = " ";
+    char *token;
+
+
+    // Get the first token
+    token = strtok(buffer, delim);
+
 
     // call strtok() repeatedly to tokenise the string on whitespace " "
     // print out each token 
-    
+    while (token != NULL) {
+        printf("%s\n", token);
+        
+        // Subsequent calls must use NULL as the first argument
+        token = strtok(NULL, delim);
+    }
+
     return 0;
 }
