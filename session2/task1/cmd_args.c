c426de843fc5834a81dedf92830882b3662d86cb
Ethan West <sqqz0154@leeds.ac.uk>
2026-05-01 08:58:09 +0000

    don't understand memory allocation for cmd args

--- session2/task1/cmd_args.c
+++ session2/task1/cmd_args.c
@@ -6,14 +6,29 @@ int main( void ) {
     char buffer[100] = "The quick brown fox jumped over the lazy dog";
 
     char **argv; // dynamic array of char*
-    int argc;
+    int argc = 0;
 
     // 1. first pass - count argc
     // don't assign argv just locate space ' ' and count tokens
-
+    char *p = buffer;
+    int in_word = 0;
+    while (*p != '\0') {
+        if (*p != ' ' && in_word==0) {
+            in_word = 1;
+            argc++;
+        } else if (*p == ' ') {
+            in_word = 0;
+        }
+        p++;
+    }
     // 2. dynamically allocate argv array
+    argv = malloc((sizeof(char*)) * argc);
+    if (argv == NULL){
+        return 1;
+    }
 
     // 3. second pass - allocate and fill argv
+  
 
     // check output for argc,argv
     printf("argc = %d\n",argc);
@@ -22,6 +37,7 @@ int main( void ) {
     }
 
     // free argv dynamic memory
+    free(argv);
 
     return 0;
 }
