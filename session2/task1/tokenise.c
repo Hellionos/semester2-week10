0d21415944e8fd23ec67937402386e4e9ca5985e
Ethan West <sqqz0154@leeds.ac.uk>
2026-05-01 07:56:43 +0000

    looked at tokenise but still don't understand

--- session2/task1/tokenise.c
+++ session2/task1/tokenise.c
@@ -10,6 +10,22 @@ int main( void ) {
     
     // iterate through each character of the string
     // every time you find a space ' ' assign a new token to argv[]
+    char *p = buffer;
+    while (*p != '\0' && argc < 10) {
+        // 1. Save the start of the current word
+        argv[argc++] = p;
+
+        // 2. Scan until we hit a space or the end of the string
+        while (*p != ' ' && *p != '\0') {
+            p++;
+        }
+
+        // 3. If we stopped at a space, null-terminate it and move to the next char
+        if (*p == ' ') {
+            *p = '\0';
+            p++;
+        }
+    }
 
     // check output for argc,argv
     printf("argc = %d\n",argc);
